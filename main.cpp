1d0060ee70d56f90ea6a45f2cf9f127e454f3f1d
jofr1815 <75509140+jofr1815@users.noreply.github.com>
2022-06-01 20:29:41 -0600

    Added collatz()
    Added a basic function to perform the collatz conjecture and output each step

--- main.cpp
+++ main.cpp
@@ -2,9 +2,34 @@
 
 using namespace std;
 
+int collatz(int num)
+{
+    cout << "Current number is: " << num << endl;
+    if (num <= 0){
+        cout << "Error, Input Number is not Positive" << endl;
+        return 2;
+    }
+    else if (num == 1){
+        // Reached one, end recursion
+        cout << "Reached one! Collatz Confirmed" << endl;
+        return num;
+    }
+    else if (num%2 == 1){
+        // Num is odd, multiply by three and add one
+        cout << "Num is odd, multiply by three and add 1" << endl;
+        num = 3*num + 1;
+        collatz(num);
+    }
+    else{
+        // Num is even, divide by two
+        cout << "Num is even, divide by two" << endl;
+        num = num/2;
+        collatz(num);
+    }
+}
+
 int main()
 {
-    cout << "Hello World!" << endl;
-    
+    cout << collatz(3);
     return 0;
 }
