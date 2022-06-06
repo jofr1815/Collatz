f65b0a46478c4e5e461b2438ed6f6ff819e568a3
jofr1815 <75509140+jofr1815@users.noreply.github.com>
2022-06-06 00:07:51 -0600

    Added Maximum Variable
    Keeps track of the largest number in the sequence, and outputs it at the end of recursion.

--- main.cpp
+++ main.cpp
@@ -3,8 +3,13 @@
 
 using namespace std;
 
-int collatz(int num, int steps)
+int collatz(int num, int steps, int max)
 {
+    // Check if current step is the first, if so the maxiumum should be the initial number
+    if (steps == 0){
+        max = num;
+    }
+
     cout << "Current number is: " << num << endl;
     if (num <= 0){
         cout << "Error, Input Number is not Positive" << endl;
@@ -14,21 +19,27 @@ int collatz(int num, int steps)
         // Reached one, end recursion
         cout << "Reached one! Collatz Confirmed" << endl;
         cout << steps << " steps were used!" << endl;
+        cout << max << " was the highest number reached!" << endl;
         return num;
     }
     else if (num%2 == 1){
         // Num is odd, multiply by three and add one
         cout << "Num is odd, multiply by three and add 1" << endl;
         num = 3*num + 1;
+
+        // Check if the new number is the new maximum
+        if (num > max){
+            max = num;
+        }
         steps++;
-        collatz(num, steps);
+        collatz(num, steps, max);
     }
     else{
         // Num is even, divide by two
         cout << "Num is even, divide by two" << endl;
         num = num/2;
         steps++;
-        collatz(num, steps);
+        collatz(num, steps, max);
     }
 }
 
@@ -45,7 +56,7 @@ int main()
         // Take in input, perform Collatz conjecture
         cout << "Enter a number: ";
         cin >> num;
-        collatz(num, 0);
+        collatz(num, 0, 0);
 
         cout << "Would you like to try another number? (y/n):" << endl;
         cin >> usrInput;
