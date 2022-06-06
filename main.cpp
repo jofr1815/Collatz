60392e4a6a6c1a04445645f5f2cd065d07be44d4
jofr1815 <75509140+jofr1815@users.noreply.github.com>
2022-06-05 23:47:38 -0600

    Added steps variable
    Counts the number of steps in the sequence to return to one, and outputs at the end of the recursion.

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-int collatz(int num)
+int collatz(int num, int steps)
 {
     cout << "Current number is: " << num << endl;
     if (num <= 0){
@@ -13,19 +13,22 @@ int collatz(int num)
     else if (num == 1){
         // Reached one, end recursion
         cout << "Reached one! Collatz Confirmed" << endl;
+        cout << steps << " steps were used!" << endl;
         return num;
     }
     else if (num%2 == 1){
         // Num is odd, multiply by three and add one
         cout << "Num is odd, multiply by three and add 1" << endl;
         num = 3*num + 1;
-        collatz(num);
+        steps++;
+        collatz(num, steps);
     }
     else{
         // Num is even, divide by two
         cout << "Num is even, divide by two" << endl;
         num = num/2;
-        collatz(num);
+        steps++;
+        collatz(num, steps);
     }
 }
 
@@ -42,7 +45,7 @@ int main()
         // Take in input, perform Collatz conjecture
         cout << "Enter a number: ";
         cin >> num;
-        collatz(num);
+        collatz(num, 0);
 
         cout << "Would you like to try another number? (y/n):" << endl;
         cin >> usrInput;
