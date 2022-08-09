bda96ef22be26edf103b519a4599e656ff73a8e6
jofr1815 <75509140+jofr1815@users.noreply.github.com>
2022-08-08 18:55:20 -0600

    Added comments to driver

--- driver.cpp
+++ driver.cpp
@@ -6,12 +6,15 @@ using namespace std;
 
 int main()
 {
+    // Create and initialize variables
     CollatzTree Tree;
     int input;
 
+    // Gather user input number
     cout << "Enter a number: " << endl;
     cin >> input;
     
+    // Check for valid input, and return one step of the collatz conjecture
     if (Tree.validInput(input)){
         int response = Tree.basicCollatz(input);
         cout << response << endl;
